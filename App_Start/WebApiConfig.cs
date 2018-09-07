68e8dcf83a421f9c1f29b12e7637bfcacc91772c
sitijumatun <sitijumatun28@gmail.com>
2018-09-08 05:51:58 +0700

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Pertamax_Sampah
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+			config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+               name: "TransaksiMasukAPI",
+               routeTemplate: "api/TransaksiMasukAPI/{iduser}/{idspbu}/{idteller}/{idsampah}/{tanggal}/{jmlsampah}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+        }
+    }
+}
